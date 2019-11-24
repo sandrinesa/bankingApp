dd2a37d8c5cde9e142eb53525eb3170ee22a8ddd
sandrinesa <58148915+sandrinesa@users.noreply.github.com>
2019-11-24 23:38:13 +0200

    Add files via upload

---
+++ bankApp/customer.cs
@@ -0,0 +1,40 @@
+namespace BankApp
+{
+    public class Customer
+    {
+        private string firstName;
+        private string lastName;
+        private string gender;
+
+        public void setFirstName(string firstName)
+        {
+            this.firstName = this.firstName;
+        }
+
+        public void setLastName(string lastName)
+        {
+            this.lastName = this.lastName;
+        }
+
+        public void setGender(string gender)
+        {
+            this.gender = this.gender;
+        }
+
+        public string getFirstName()
+        {
+            return firstName;
+        }
+        
+        public string getLasttName()
+        {
+            return lastName;
+        }
+
+        public string getGender()
+        {
+            return gender;
+        }
+
+    }
+}
